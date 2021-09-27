a9af137f6957321c57be6b6890aba01713868531
suhailxyz <suhailj17@gmail.com>
2021-09-26 23:05:21 -0400

    lab 4 carousel
    buttons unstyled

---
+++ client/lab_4/script.js
@@ -0,0 +1,45 @@
+let slidePosition = 0;
+const slides = document.querySelectorAll(".carousel_item");
+const totalSlides = slides.length;
+
+document.
+    querySelector("#carousel_button--next")
+    .addEventListener("click", function(){
+        moveToNext();
+
+    })
+
+document.
+querySelector("#carousel_button--back")
+.addEventListener("click", function(){
+    moveBack();
+
+})
+
+function updatePosition() {
+    for (let slide of slides) {
+        slide.classList.remove('carousel_item--visible');
+        slide.classList.add('carousel_item--hidden');
+
+    }
+
+    slides[slidePosition].classList.add('carousel_item--visible')
+}
+
+function moveToNext(){
+    if (slidePosition === totalSlides-1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updatePosition();
+}
+
+function moveBack() {
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updatePosition();
+}
